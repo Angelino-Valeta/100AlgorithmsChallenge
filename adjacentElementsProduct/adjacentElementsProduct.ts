bde38e1c2219c1c9b26f4f11427b69860147bbca
Angelino Valeta <angelinovaleta@gmail.com>
2021-11-04 20:53:54 +0100

    Adjacent Elements Product

---
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -0,0 +1,11 @@
+function adjacentElementsProduct(inputArray: number[]): number{
+  let largestNumber = inputArray[0] * inputArray[1];
+
+  inputArray.map((number,i) => {
+    largestNumber = number * inputArray[i + 1] > largestNumber ? number * inputArray[i+1]: largestNumber; 
+  })
+
+  return largestNumber
+}
+
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
