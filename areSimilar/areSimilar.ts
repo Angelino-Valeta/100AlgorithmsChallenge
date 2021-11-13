eee925eb4355a257ce81d666ab6299723f7b46af
Angelino Valeta <angelinovaleta@gmail.com>
2021-11-13 20:54:27 +0100

    Are Similar

---
+++ areSimilar/areSimilar.ts
@@ -0,0 +1,7 @@
+function areSimilar(a: number[], b: number[]): boolean {
+   return a.sort().join('') === b.sort().join('');
+}
+
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));
